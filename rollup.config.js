4dd1b1fc065563fd9db29266a9bc5ee12ed72991
john.mortlock <john.mortlock@gmail.com>
2021-12-07 08:33:12 +1030

    Configure rollup build

---
+++ rollup.config.js
@@ -0,0 +1,38 @@
+import babel from 'rollup-plugin-babel';
+import istanbul from 'rollup-plugin-istanbul';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+let pkg = require('./package.json');
+
+let plugins = [
+  babel({
+    exclude: 'node_modules/**',
+  }),
+  nodeResolve(),
+];
+
+if (process.env.BUILD !== 'production') {
+  plugins.push(
+    istanbul({
+      exclude: ['test/**/*', 'node_modules/**/*', 'lib/**/*'],
+    })
+  );
+}
+
+export default {
+  input: 'lib/index.js',
+  plugins: plugins,
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+      name: 'vehiclelink-js',
+      sourcemap: true,
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+};
