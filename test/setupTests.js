28b663a91e9b7ecc715ea01b24067072191a7a0a
damon.shi <damon.shi@sealink.com.au>
2021-12-08 11:24:38 +1030

    [RB-96] Rename api and clean code

--- test/setupTests.js
+++
@@ -1,7 +0,0 @@
-const nodeCrypto = require('crypto');
-require('regenerator-runtime/runtime');
-window.crypto = {
-  getRandomValues: function (buffer) {
-    return nodeCrypto.randomFillSync(buffer);
-  },
-};
